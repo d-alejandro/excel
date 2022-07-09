aca95a2bd7dd6e831f0589873c64ff815b2c0337
Alexander Dadyka <sanekokokok@gmail.com>
2022-07-09 21:53:50 +0300

    Add: redis to database config file

--- config/database.php
+++ config/database.php
@@ -63,6 +63,16 @@ return [
             ]) : [],
         ],
 
+        'redis' => [
+            'client' => env('REDIS_CLIENT', 'phpredis'),
+            'default' => [
+                'host' => env('REDIS_HOST', '127.0.0.1'),
+                'password' => env('REDIS_PASSWORD', null),
+                'port' => env('REDIS_PORT', 6379),
+                'database' => env('REDIS_DB', 0),
+            ],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
