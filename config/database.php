2de24cda0a296ada5e2ffad6d650bd88e4639f3b
Alexander Dadyka <sanekokokok@gmail.com>
2022-07-10 23:08:54 +0300

    Fix: configs

--- config/database.php
+++ config/database.php
@@ -63,16 +63,6 @@ return [
             ]) : [],
         ],
 
-        'redis' => [
-            'client' => env('REDIS_CLIENT', 'phpredis'),
-            'default' => [
-                'host' => env('REDIS_HOST', '127.0.0.1'),
-                'password' => env('REDIS_PASSWORD', null),
-                'port' => env('REDIS_PORT', 6379),
-                'database' => env('REDIS_DB', 0),
-            ],
-        ],
-
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
@@ -129,7 +119,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
